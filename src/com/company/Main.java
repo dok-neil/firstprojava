bab6a36d79add78b3c1f4b4b73cade789a33c165
dok-neil <kernel618@gmail.com>
2021-05-27 19:27:17 +0800

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int number = 100;
+        String name = "kram";
+        System.out.println(number);
+        System.out.println(name);
+    }
+
+
+
+        private class Student{
+            private Long id;
+            private String name;
+            private String email;
+            private LocalDate dob;
+            private Integer age;
+
+            public Student(){
+
+            }
+                Long id;
+                String name;
+                String email;
+                LocalDate dob;
+                Integer age;
+        }
+}
